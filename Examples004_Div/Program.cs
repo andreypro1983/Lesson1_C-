414e25b848654a9a04e0a46f2fc847b31eaea0d9
Prozhikin_Andrey <a.prozhikin@gmail.com>
2022-11-22 19:56:22 +0200

    Feat:Change Examples 004 Div

--- Examples004_Div/Program.cs
+++ Examples004_Div/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double numberA=12;
+double numberB=5;
+double Div=numberA/numberB;
+Console.WriteLine ("Частное чисел "+numberA+" и "+numberB+ " равно "+Div);
