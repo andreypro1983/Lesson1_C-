e4fbd05ce13fa570f8dae392eac9035269c37986
Prozhikin_Andrey <a.prozhikin@gmail.com>
2022-11-22 21:50:56 +0200

    feat: Add projects from second lecture

---
+++ Examples008_IntroMethod/Program.cs
@@ -0,0 +1,25 @@
+﻿int a1 = 15;
+int a2 = 100;
+int a3 = 17;
+int b1 = 33;
+int b2 = 13;
+int b3 = 170;
+int c1 = 10;
+int c2 = 24;
+int c3 = 11;
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+// int max1 = Max(a1, b1, c1);
+// int max2 = Max(a2, b2, c2);
+// int max3 = Max(a3, b3, c3);
+// int max = Max(max1, max2, max3);
+
+int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
+Console.WriteLine(max);
---
+++ Examples010_MethodArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = { 5, 17, 2, 4, 10, 25, 47, 2, 6 };
+int n = array.Length;
+int find = 2;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+
+    index++;
+}
